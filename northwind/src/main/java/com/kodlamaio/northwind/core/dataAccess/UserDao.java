2078d7d6bd1659109744d1025622f1377311b983
sarpkansavaskan00 <sarpkansavaskan@gmail.com>
2021-09-12 23:10:14 +0300

    last update

---
+++ northwind/src/main/java/com/kodlamaio/northwind/core/dataAccess/UserDao.java
@@ -0,0 +1,9 @@
+package com.kodlamaio.northwind.core.dataAccess;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kodlamaio.northwind.core.entities.User;
+
+public interface UserDao extends JpaRepository<User, Integer>{
+	User findByEmail(String email);
+}
